bdb762ba08d903e1ba218292220bd87cfb1b154e
Jared Mohney <jared.mohney@gmail.com>
2018-10-28 19:56:11 -0600

    setting up ebay string

--- server/index.js
+++ server/index.js
@@ -9,6 +9,23 @@ app.get('/test', (req, res) => {
     res.send('Working!');
 });
 
+app.get('/ebay', (req, res) => {
+
+    const OPERATION_NAME = "findItemsByKeywords";
+    const SERVICE_VERSION = "1.0.0";
+    const SECURITY_APPNAME = "";
+    const GLOBAL_ID = "EBAY-US";
+    const RESPONSE_DATA_FORMAT = "JSON";
+    const CALLBACK = "_cb_findItemsByKeywords";
+    const KEYWORDS = "barbie doll";
+    const PAGINATION = "3";
+
+    
+
+
+    // https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=iPhone&paginationInput.entriesPerPage=6&GLOBAL-ID=EBAY-US&siteid=0
+});
+
 app.listen(8000, () => console.log('Listening to port 8000!'));
 
 //yerp
